d076b4757c86e9a1d56842e6969a69c4f8902d7d
Andrey <anovikov@gridgain.com>
2015-09-08 09:54:48 +0700

    # GG-843 Updated agent.

--- modules/control-center-web/src/main/js/controllers/common-module.js
+++ modules/control-center-web/src/main/js/controllers/common-module.js
@@ -1501,7 +1501,7 @@ controlCenterModule.controller('agent-download', [
 
             var lnk = document.createElement('a');
 
-            lnk.setAttribute('href', '/agent/ignite-web-agent-1.4.1-SNAPSHOT.zip');
+            lnk.setAttribute('href', '/agent/ignite-web-agent-1.5.0-SNAPSHOT.zip');
             lnk.style.display = 'none';
 
             document.body.appendChild(lnk);
